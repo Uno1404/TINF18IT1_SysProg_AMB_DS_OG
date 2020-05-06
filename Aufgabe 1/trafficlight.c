2d555cbf5a570671ab13aa3c6b59b673123a7ecf
mariusalexander <52258575+mariusalexander@users.noreply.github.com>
2020-05-06 17:45:00 +0200

    udpate trafficlight

--- Aufgabe 1/trafficlight.c
+++ Aufgabe 1/trafficlight.c
@@ -15,49 +15,46 @@
 	3 LED (3 farbig) Eine Ampelschaltung	-> Aufgaben/trafficlight.h
 		Am Anfang 1000ms rot
 		Dann 100ms Rot-Gelb
-		Dann 1000 Grün
+		Dann 1000 GrÃ¼n
 		Dann 100ms gelb
 		Dann wdh
 */
 
 
-// traffic_light - setup
-void setup()
+// traffic_light
+inline void loop()
 {
-	SET_PIN_OUTPUT(DDRB, PORTB4);	// rot
-	SET_PIN_OUTPUT(DDRB, PORTB3);	// gelb
-	SET_PIN_OUTPUT(DDRB, PORTB2);	// grün
-}
-
-// traffic_light - loop
-void loop()
-{
-		// 1000ms: red
-		SET_PIN_HIGH(PORTB, PORTB4);
-		_delay_ms(1000);
-		
-		// 100ms:  red + yellow
-		SET_PIN_HIGH(PORTB, PORTB3);
-		_delay_ms(100);
-		
-		// 1000ms: green
-		SET_PIN_LOW(PORTB, PORTB4);
-		SET_PIN_LOW(PORTB, PORTB3);
-		SET_PIN_HIGH(PORTB, PORTB2);
-		_delay_ms(1000);
-		
-		// 100ms:  red + yellow
-		SET_PIN_LOW(PORTB, PORTB2);
-		SET_PIN_HIGH(PORTB, PORTB3);
-		_delay_ms(100);
-		
-		SET_PIN_LOW(PORTB, PORTB3);
+	// 1000ms: red
+	SET_PIN_HIGH(PORTB, PORTB4);
+	_delay_ms(1000);
+	
+	// 100ms:  red + yellow
+	SET_PIN_HIGH(PORTB, PORTB3);
+	_delay_ms(100);
+	
+	// 1000ms: green
+	SET_PIN_LOW(PORTB, PORTB4);
+	SET_PIN_LOW(PORTB, PORTB3);
+	SET_PIN_HIGH(PORTB, PORTB2);
+	_delay_ms(1000);
+	
+	// 100ms:  red + yellow
+	SET_PIN_LOW(PORTB, PORTB2);
+	SET_PIN_HIGH(PORTB, PORTB3);
+	_delay_ms(100);
+	
+	SET_PIN_LOW(PORTB, PORTB3);
 }
 
 int main()
 {
-	setup();
+	// -- Setup --
 	
+	SET_PIN_OUTPUT(DDRB, PORTB4);	// rot
+	SET_PIN_OUTPUT(DDRB, PORTB3);	// gelb
+	SET_PIN_OUTPUT(DDRB, PORTB2);	// grÃ¼n
+	
+	// -- Loop --
 	while (1)
 	{
 		loop();

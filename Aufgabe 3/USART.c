ed01bb35b24369f4e938832188fa7674424f1cef
DominikSeus <43820787+DominikSeus@users.noreply.github.com>
2020-05-06 17:24:54 +0200

    Add files via upload

---
+++ Aufgabe 3/USART.c
@@ -0,0 +1,44 @@
+/*
+ * USART.c
+ *
+ * Created: 25.04.2020 11:47:45
+ *  Author: MariusAlexander
+ */
+
+#include "USART.h"
+
+ void usart_init()
+ {
+	 UBRR0H  = (MYUBRR << 8);
+	 UBRR0L  = MYUBRR;
+ 
+	 UCSR0B |= (1 << RXEN0) | (1 << TXEN0);		// enable receiver and transmitter
+	 UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);	// set frame 8data, 1stp
+ }
+
+ void usart_send(uint8_t data)
+ {
+	 while (!(UCSR0A & (1 << UDRE0)));
+	 
+	 UDR0 = data;
+ }
+
+ void usart_send_string(char* data)
+ {
+	 for (int i = 0; ; i++)
+	 {
+		 if (data[i] == 0)
+		 {
+			 break;
+		 }
+		 
+		 usart_send(data[i]);
+	 }
+ }
+
+ uint8_t usart_receive()
+ {
+	 while (!(UCSR0A & (1 << RXC0)));
+	 
+	 return (uint8_t) UDR0;
+ }

8df82d1c05df6be9716c47bb7605199137362b9e
DominikSeus <43820787+DominikSeus@users.noreply.github.com>
2020-05-14 19:09:07 +0200

    :)

--- Aufgabe_3_lauflicht_10bit/Aufgabe_3_lauflicht_10bit/lauflicht_10bit.c
+++ Aufgabe_3_lauflicht_10bit/Aufgabe_3_lauflicht_10bit/lauflicht_10bit.c
@@ -2,7 +2,6 @@
  *  Author: Broecker, Goel, Seus
  */
 
-#define F_CPU 16000000
 
 #include "lauflicht_10bit.h"
 
@@ -10,42 +9,42 @@
 
 char direction = LEFT;
 
-void lauflicht_10bit_init(){
+void lauflicht_10bit_init() {
 	//DDRB = 0b00001111;
-	SET_PIN_OUTPUT(DDRB, 3);
-	SET_PIN_OUTPUT(DDRB, 2);
-	SET_PIN_OUTPUT(DDRB, 1);
-	SET_PIN_OUTPUT(DDRB, 0);
+	SET_PIN_OUTPUT(DDRB, DDB3);
+	SET_PIN_OUTPUT(DDRB, DDB2);
+	SET_PIN_OUTPUT(DDRB, DDB1);
+	SET_PIN_OUTPUT(DDRB, DDB0);
 	
 	//DDRD = 0b11111100;
-	SET_PIN_OUTPUT(DDRD, 7);
-	SET_PIN_OUTPUT(DDRD, 6);
-	SET_PIN_OUTPUT(DDRD, 5);
-	SET_PIN_OUTPUT(DDRD, 4);
-	SET_PIN_OUTPUT(DDRD, 3);
-	SET_PIN_OUTPUT(DDRD, 2);
+	SET_PIN_OUTPUT(DDRD, DDD7);
+	SET_PIN_OUTPUT(DDRD, DDD6);
+	SET_PIN_OUTPUT(DDRD, DDD5);
+	SET_PIN_OUTPUT(DDRD, DDD4);
+	SET_PIN_OUTPUT(DDRD, DDD3);
+	SET_PIN_OUTPUT(DDRD, DDD2);
 	
 	//initial state: Turn on the LED at the right border and set the floating direction to LEFT ('l')
-	SET_PIN_HIGH(START_PORT, START_NUMBER);
+	SET_PIN_HIGH(START_PORT, START_PIN);
 }
 
-void moveLeft(){
+void moveLeft() {
 	//if PortB is 0, the light is at register d, and vice versa. (PortD can not be 0 because the least significant bit of it is a status bit which we may not change)
-	if (PORTB == 0){
+	if (PORTB == 0) {
 		//after the bitshift we have to add 1 again to maintain the status of the lsb
-		PORTD = (PORTD >> 1) +1;
+		PORTD = (PORTD >> 1) + 1;
 	}
-	else if (PORTD == 1){
+	else if (PORTD == 1) {
 		PORTB = PORTB >> 1;
 	}
 }
 
-void moveRight(){
-	if (PORTB == 0){
+void moveRight() {
+	if (PORTB == 0) {
 		//when bitshifting to the left we first have to nullify the status bit, because we would shift it which would cause unexpected results. After shifting we add 1 again
-		PORTD = ((PORTD-1) << 1) +1;
+		PORTD = ((PORTD-1) << 1) + 1;
 	}
-	else if (PORTD == 1){
+	else if (PORTD == 1) {
 		PORTB = PORTB << 1;
 	}
 }
@@ -53,24 +52,24 @@ void moveRight(){
 void lauflicht_10bit_loop() {
 	_delay_ms(TIME_DELAY);
 	//check if the left border is reached and change the direction to right
-	if (BORDER_LEFT){
+	if (BORDER_LEFT) {
 		direction = RIGHT;
 	}
 	//change the direction to left if the right border is reached
-	else if (BORDER_RIGHT){
+	else if (BORDER_RIGHT) {
 		direction = LEFT;
 	}
 	//check if transition state is reached. If so: toggle both of the Pins at the border between both of the registers
-	else if (TRANSITION){
-		TOGGLE_PIN(PORTB, 0);
-		TOGGLE_PIN(PORTD,7);
+	else if (TRANSITION) {
+		TOGGLE_PIN(PORTB, PORTB0);
+		TOGGLE_PIN(PORTD, PORTD7);
 		_delay_ms(TIME_DELAY);
 	}
 	//check the direction
-	if (direction == LEFT){
+	if (direction == LEFT) {
 		moveLeft();
 	}
-	else if (direction == RIGHT){
+	else if (direction == RIGHT) {
 		moveRight();
 	}
 }
--- Aufgabe_3_lauflicht_10bit/Aufgabe_3_lauflicht_10bit/lauflicht_10bit.h
+++ Aufgabe_3_lauflicht_10bit/Aufgabe_3_lauflicht_10bit/lauflicht_10bit.h
@@ -5,22 +5,27 @@
 #ifndef _10BITLAUFLICHT_H_
 #define _10BITLAUFLICHT_H_
 
+#define F_CPU 16000000UL
+
 //Keywords to describe the lights direction:
-#define LEFT 'l'
-#define RIGHT 'r'
+#define LEFT	'l'
+#define RIGHT	'r'
+
+#define START_PORT	PORTB
+#define START_PIN	PORTB3
+
+#define PIN_RIGHT	PORTD2
+#define PIN_LEFT	PORTB3
 
 //Macro to check if the right respectively the left Border is reached
-#define BORDER_RIGHT (PORTB == (1 << 3))
-#define BORDER_LEFT (PORTD == ((1 << 2)+1))
+#define BORDER_RIGHT	(PORTB ==  (1 << PIN_LEFT))
+#define BORDER_LEFT		(PORTD == ((1 << PIN_RIGHT) + 1))
 
 //Macro to determine if the transition state between register b and register d is reached
-#define TRANSITION ((PORTB & 1) | (PORTD & (1 << 7)))
+#define TRANSITION		((PORTB & 1) | (PORTD & (1 << 7)))
 
-//function to start 10bitfloatinglight
-#define START_PORT PORTB
-#define START_NUMBER 3
 
-#define TIME_DELAY 50
+#define TIME_DELAY		100
 
 void lauflicht_10bit_init();
 void lauflicht_10bit_loop();
